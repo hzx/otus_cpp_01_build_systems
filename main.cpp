f91921cc1a9ec07ddcb13bb902118166140dec37
Dmitry Dementev <dem@oldschooldev.com>
2019-04-12 09:58:09 +0300

    use spdlog; flag for boost to use cxx11 ABI

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "lib.h"
 
-/* #include <spdlog/spdlog.h> */
-#include <iostream>
+#include <spdlog/spdlog.h>
 
 int main()
 {
-    /* auto logger = spdlog::stdout_logger_mt("console"); */
-    /* logger->info("version {} was started", version()); */
-    std::cout << "version " << version() << " was started" << std::endl;
+    auto logger = spdlog::stdout_logger_mt("console");
+    logger->info("version {} was started", version());
 
     return 0;
 }
