97a47901b41ffd9ac46e6475bd1a82dffa307942
Dmitry Dementev <dem@oldschooldev.com>
2019-04-12 09:50:54 +0300

    use iostream, no spblog

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+/* #include <spdlog/spdlog.h> */
+#include <iostream>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    /* auto logger = spdlog::stdout_logger_mt("console"); */
+    /* logger->info("version {} was started", version()); */
+    std::cout << "version " << version() << " was started" << std::endl;
 
     return 0;
 }
